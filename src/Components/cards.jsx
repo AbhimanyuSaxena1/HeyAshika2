035cca6a813fe7d862a99e413d7fd9d2b1816c24
abhimanyusaxena1 <abhimanyusaxena1>
2025-02-23 01:57:59 +0530

    fix: enhance text shadow effects and update border styles in cards component

--- src/Components/cards.jsx
+++ src/Components/cards.jsx
@@ -9,11 +9,13 @@ import ILY from "../assets/ILY.mp3";
 function Cards() {
   const [val, setVal] = useState(false);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [shadowColor, setShadowColor] = useState("#f75590");
   const images = [img1, img2, img3, img4];
   const audioRef = useRef(null);
 
   const handleSpanClick = () => {
     setVal(!val);
+    setShadowColor(val ? "#f75590" : "#FCE4DB");
     if (audioRef.current) {
       audioRef.current.src = ILY;
       audioRef.current.play();
@@ -32,37 +34,26 @@ function Cards() {
       />
       <h1
         className={`${val === false ? "text-[#f75590]" : "text-[#FCE4DB]"} z-20 text-7xl stroke-black font-["Salita"] absolute top-[13%] lg:top-[20%] lg:text-8xl lg:left-[10%]`} 
-        style={{ textShadow: `${val === false ? '2px 2px 0 #FCE4DB, -2px -2px 0 #FCE4DB' : '2px 2px 0 #f75590, -2px -2px 0 #f75590'}` }}
+        style={{ textShadow: `${val === false ? '2px 2px 5px #FCE4DB, -2px -2px 5px #FCE4DB' : '2px 2px 5px #f75590, -2px -2px 5px #f75590'}` }}
       >
         Hey
       </h1>
       <h1
         className={`${val === false ? "text-[#FCE4DB]" : "text-[#f75590]"} z-20 text-8xl rounded-full font-["Salita"] absolute top-[20%] lg:top-[30%] lg:text-9xl lg:left-[10%]`}
-        style={{ textShadow: `${val === false ? '2px 2px 0 #f75590, -2px -2px 0 #f75590' : '2px 2px 0 #FCE4DB, -2px -2px 0 #FCE4DB'}` }}
+        style={{ textShadow: `${val === false ? '2px 2px 5px #f75590, -2px -2px 5px #f75590' : '2px 2px 5px #FCE4DB, -2px -2px 5px #FCE4DB'}` }}
       >
         Palak
       </h1>
       <h1
         className={`${val === false ? "text-[#FCE4DB]" : "text-[#f75590]"} z-20 text-4xl rounded-full font-["Salita"] absolute bottom-[20%] lg:top-[45%] lg:text-4xl lg:left-[10%]`}
+        style={{ textShadow: `2px 2px 5px ${shadowColor}, -2px -2px 5px ${shadowColor}` }}
       >
         I love you so much❤️
       </h1>
-      <div
-        className={`rounded-xl w-75 h-100 ${
-          val === false ? "border-[#f75590]" : "border-[#FCE4DB]"
-        } border-6 border-dashed relative overflow-hidden flex justify-center items-center`}
-      >
-        <div
-          className='w-full h-full flex transition-transform duration-1000 transform'
-          style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
-        >
+      <div className={`rounded-xl w-75 h-100 ${val === false ? "border-[#f75590]" : "border-[#FCE4DB]"} border-6 border-dashed relative overflow-hidden flex justify-center items-center`} style={{ boxShadow: `0 0 15px 5px ${val === false ? '#FCE4DB' : '#f75590'}` }}>
+        <div className='w-full h-full flex transition-transform duration-1000 transform' style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}>
           {images.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              className='object-cover h-full w-full transition-all duration-1000'
-              alt={`image-${index}`}
-            />
+            <img key={index} src={image} className='object-cover h-full w-full transition-all duration-1000' alt={`image-${index}`} />
           ))}
         </div>
         <span
